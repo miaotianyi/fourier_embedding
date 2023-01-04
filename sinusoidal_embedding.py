070ec68b82f0d66e2a18db6b9153082f3ace4cb6
Tianyi Miao <tennielmiao@gmail.com>
2023-01-04 10:20:23 -0500

    allow periods argument

--- sinusoidal_embedding.py
+++ sinusoidal_embedding.py
@@ -4,7 +4,9 @@ from torch import nn
 
 
 class SinusoidalEmbedding(nn.Module):
-    def __init__(self, embedding_dim, min_period=2*math.pi, max_period=10000*2*math.pi, progression="geometric"):
+    def __init__(self, embedding_dim=None,
+                 min_period=2*math.pi, max_period=10000*2*math.pi, progression="geometric",
+                 periods=None):
         """
         A non-trainable element-wise sinusoidal embedding layer
         that maps each real-valued scalar entry to a vector.
@@ -12,21 +14,39 @@ class SinusoidalEmbedding(nn.Module):
         - Input: `(*)`, tensor of arbitrary shape containing the entries to encode.
         - Output: `(*, embedding_dim)`, where `*` is the input shape.
 
+        This embedding is performed element-wise.
+        In short, a list of different angular frequencies `w` is generated;
+        we compute `sin(wx)` and `cos(wx)` for each `w` and
+        concatenate them along the embedding dimension.
+
         Output embedding size `embedding_dim` should be an even number `2*half_dim`.
         The output tensor `PE` has 2 components: `PE_sin` and `PE_cos`.
         They both have length `half_dim` in the last (-1) dimension.
         They are then concatenated to `PE = [PE_sin, PE_cos]`
         of length `embedding_dim` in the last (-1) dimension.
 
-        This embedding is performed element-wise.
-        For any scalar input `x`, sinusoidal functions `sin(x/n)` and `cos(x/n)`
-        have period (wavelength) `2pi*n`.
-        (Intuitive naming convention for those familiar with Fourier analysis.)
-        The minimum and maximum period lengths are specified as hyperparameters,
-        which are then used to derive `n = period/(2*pi)`.
-        We then create `half_dim` different `n`s ranging from `min_n` to `max_n`
-        according to a geometric or arithmetic progression.
-        These `n`s will be used to determine wavelengths in `PE_sin` and `PE_cos`.
+        For a periodic function `g(x)`, the linear period `T` is defined as
+        the length of a complete cycle, such that :math:`g(x)=g(x+T)` for all `x`.
+        The linear frequency :math:`f=1/T` counts the number of periods
+        in a unit (1) interval of `x`.
+        In a sinusoid, the angular frequency :math:`\omega=2\pi f=2\pi/T` is
+        defined as the number of radians rotated per unit interval of `x`.
+        Angular frequency is helpful because `sin(wx)` and `cos(wx)` have
+        angular frequency `w`, linear frequency `f`, and linear period `T`.
+
+        There are 4 ways to generate angular frequencies `w`.
+        Only one of the following should be used at runtime:
+
+        1. Use the `periods` keyword argument to pass in a list of linear periods
+        2. Generate an arithmetic/geometric progression of `half_dim` linear periods
+           from `min_period` to `max_period` inclusive.
+        3. Use the `frequencies` keyword argument to pass in a list of linear frequencies
+        4. Generate an arithmetic/geometric progression of `half_dim` linear frequencies
+           from `min_freq` to `max_freq` inclusive.
+
+        We use linear frequency and period as hyperparameters instead of angular,
+        because they let us think in terms of the periodicity in input feature x,
+        which rarely relates to :math:`2\pi` in practice.
 
         Sinusoidal embedding for positional encoding is popularized by
         the Transformer architecture to address the order-invariance of attention
@@ -35,7 +55,7 @@ class SinusoidalEmbedding(nn.Module):
         this vector embedding is then added with a learned embedding of the same length.
 
         Let's see how the original sinusoidal embedding works in Transformer:
-        Taking `min_n=1, max_n=10000, progression="geometric"` for example (as in Transformer),
+        Taking `min_period=2*math.pi, max_period=10000*2*math.pi, progression="geometric"`,
         for any input scalar `x`,
         `PE_sin(x)[i] = sin(x / 10000^{i/(half_dim-1)})`;
         `PE_cos(x)[i] = cos(x / 10000^{i/(half_dim-1)})`.
@@ -46,15 +66,22 @@ class SinusoidalEmbedding(nn.Module):
 
         This code is inspired by HuggingFace (https://huggingface.co/blog/annotated-diffusion),
         which we use as a benchmark for comparison.
-        We generalize this embedding layer, so it applies to
-        arbitrary real-valued input tensors (not just [N]-shaped sequences).
-        We also generalize it to arbitrary `min_n`, `max_n`, and `progression` modes
-        (Transformer uses geometric progression, we also allow arithmetic progression).
+
+        We generalize this sinusoidal embedding layer in the following ways:
+
+        1. It applies to arbitrary real-valued input tensors, not just [N]-shaped sequences.
+        2. It allows arbitrary `min_period` and `max_period` (or `max_freq` and `min_freq`)
+           and progression modes (arithmetic/geometric) to generate a sequence of frequencies,
+           while Transformer uses geometric progression from `w=1` to `w=10000` angular frequencies.
+        3. You can pass in your own list of linear frequencies/periods.
 
         Parameters
         ----------
-        embedding_dim : int
+        embedding_dim : int, default: None
             The length of each embedding vector.
+
+            If None, `periods` argument must be provided.
+            Then `embedding_dim` will be inferred as `2*len(periods)`.
 
         min_period : float, default: 2*math.pi
             Constant to control the minimum wavelength (period), which is `n*2pi`.
@@ -66,21 +93,34 @@ class SinusoidalEmbedding(nn.Module):
             Constant to control the maximum wavelength (period), which is `n*2pi`.
 
             The original transformer paper uses n=10000.
+
+        progression : str, default: "geometric"
+            One of "geometric" or "arithmetic" to indicate progression type
+            from min_period to max_period.
+
+        periods : list of float, default: None
+            A list of wavelengths (periods) for sinusoidal embedding.
+
+            This argument will override all previous arguments.
+            For example, `embedding_dim` will become `2*len(periods)` because there's
+            one sine and one cosine transformation for each wavelength.
         """
         super().__init__()
-        assert embedding_dim % 2 == 0, "embedding_dim must be even for sinusoidal embedding"
-        self.embedding_dim = embedding_dim
-        assert 0 < min_period < max_period, "min_period must be less than max_period; they both need to be positive"
-        self.min_period, self.max_period = min_period, max_period
-        self.min_n = min_period / (2 * math.pi)
-        self.max_n = max_period / (2 * math.pi)
-        assert progression in ("geometric", "arithmetic"), "progression mode must be either geometric or arithmetic"
-        self.progression = progression
+        if periods is None:     # use geometric/arithmetic progression to generate periods
+            self.periods = None
+            assert embedding_dim > 0, "embedding_dim must be positive"
+            assert embedding_dim % 2 == 0, "embedding_dim must be even for sinusoidal embedding"
+            self.embedding_dim = int(embedding_dim)
+            assert 0 < min_period < max_period, "min_period must be less than max_period; they both need to be positive"
+            self.min_period, self.max_period = min_period, max_period
+            self.min_n = min_period / (2 * math.pi)
+            self.max_n = max_period / (2 * math.pi)
+            assert progression in ("geometric", "arithmetic"), "progression mode must be either geometric or arithmetic"
+            self.progression = progression
+        else:
+            self.periods = [float(p) for p in periods]
 
     def forward(self, x: torch.Tensor):  # output shape: x.shape + [dim]
-        device = x.device
-        half_dim = self.embedding_dim // 2
-
         # original formula:
         # divisor = self.max_n ** (torch.arange(half_dim, device=device) / (half_dim - 1))
         # half_embeddings = x[..., None] / divisor
@@ -95,12 +135,20 @@ class SinusoidalEmbedding(nn.Module):
         # My implementation
         # supports min_n, max_n, and geometric/arithmetic progression
         # uses linspace instead of arange; this makes exp log more useful
-        if self.progression == "geometric":
-            multiplier = torch.exp(torch.linspace(-math.log(self.min_n), -math.log(self.max_n), steps=half_dim, device=device))
-        elif self.progression == "arithmetic":
-            multiplier = 1 / torch.linspace(self.min_n, self.max_n, steps=half_dim, device=device)
+        # multiplier is (2 pi / period), so it can be multiplied with x before taking sine/cosine
+        if self.periods is None:
+            half_dim = self.embedding_dim // 2
+            if self.progression == "geometric":
+                multiplier = torch.exp(torch.linspace(-math.log(self.min_n), -math.log(self.max_n), steps=half_dim,
+                                                      device=x.device))
+            elif self.progression == "arithmetic":
+                multiplier = 1 / torch.linspace(self.min_n, self.max_n, steps=half_dim,
+                                                device=x.device)
+            else:
+                raise ValueError(f"Unknown progression mode {self.progression}")
         else:
-            raise ValueError(f"Unknown progression mode {self.progression}")
+            multiplier = (2*math.pi) / torch.tensor(self.periods, device=x.device)
+            print("periods multiplier dtype", multiplier.dtype)
         half_embeddings = x[..., None] * multiplier
 
         embeddings = torch.cat((half_embeddings.sin(), half_embeddings.cos()), dim=-1)
