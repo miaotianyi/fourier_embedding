bc29cf47e7c9a7fb4a637a0558edf164e0e1685f
Tianyi Miao <tennielmiao@gmail.com>
2023-02-08 10:30:28 -0500

    remove custom depthwise

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,12 +1,12 @@
 from .running_norm import RunningNorm
 from .sinusoidal_encoding import SinusoidalEncoding
 from .fourier_features import FourierFeatures
-from .depthwise import Depthwise1d, CustomDepthwise1d
+from .depthwise import Depthwise1d
 
 
 __all__ = [
     "RunningNorm",
     "SinusoidalEncoding",
     "FourierFeatures",
-    "Depthwise1d", "CustomDepthwise1d"
+    "Depthwise1d"
 ]
